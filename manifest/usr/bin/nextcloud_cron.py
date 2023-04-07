8c5fab043199a69268b702369c59958caf508411
Robert Schumann <robert@schumann.link>
2023-04-08 00:13:18 +0200

    fix: cleanup env handling and script

--- manifest/usr/bin/nextcloud_cron.py
+++ manifest/usr/bin/nextcloud_cron.py
@@ -17,23 +17,26 @@ except client.ApiException as e:
     print(e)
     exit(11)
 
+label_name = getenv("LABEL_APP_NAME", None)
+label_component = getenv("LABEL_APP_COMPONENT", None)
+namespace = getenv("K8S_NAMESPACE", None)
+container = getenv("CONTAINER_NAME", None)
 
-label_selector = ",".join(
-    ["app.kubernetes.io/name=nextcloud", "app.kubernetes.io/component=nextcloud"]
+if None in [label_name, label_component, namespace, container]:
+    raise ValueError("environment variables missing")
+
+selector = ",".join(
+    [
+        f"app.kubernetes.io/name={label_name}",
+        f"app.kubernetes.io/component={label_component}",
+    ]
 )
-namespace = getenv("K8S_NAMESPACE", False)
-
-if not namespace:
-    raise ValueError("namespace variable K8S_NAMESPACE missing")
 
 try:
-    resp = k8s.list_namespaced_pod(namespace=namespace, label_selector=label_selector)
+    resp = k8s.list_namespaced_pod(namespace=namespace, label_selector=selector)
 
-    for x in resp.items:
-        print(x)
-        name = x.spec.hostname
-        print(name)
-
+    pods = [x.spec.hostname for x in resp.items if x.spec.hostname]
+    for name in pods:
         resp = k8s.read_namespaced_pod(name=name, namespace=namespace)
 
         # https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/background_jobs_configuration.html#cron
@@ -47,7 +50,7 @@ try:
             k8s.connect_get_namespaced_pod_exec,
             name,
             namespace,
-            container="nextcloud",
+            container=container,
             command=exec_command,
             stderr=True,
             stdin=False,
