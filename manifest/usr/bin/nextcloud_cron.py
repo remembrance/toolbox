9702d4ba540d097b4c1117e1dd76806c18659d13
Robert Schumann <robert@schumann.link>
2023-04-07 23:14:51 +0200

    fix: add container name to nextcloud nextcloud_cron

--- manifest/usr/bin/nextcloud_cron.py
+++ manifest/usr/bin/nextcloud_cron.py
@@ -45,11 +45,13 @@ try:
             k8s.connect_get_namespaced_pod_exec,
             name,
             namespace,
+            container_name="nextcloud",
             command=exec_command,
             stderr=True,
             stdin=False,
             stdout=True,
             tty=False,
+            _preload_content=False,
         )
 
         print(
